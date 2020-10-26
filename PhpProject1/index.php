91ec762d51255e952cf7bdd6045376b173ce20ce
Danial Muhammad <ranadanialdon@gmail.com>
2020-10-26 16:05:35 +0100

    Project

---
+++ PhpProject1/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>DÚ</title>
+</head>
+<body style="background-color: rgba(143, 196, 45, 0.849)">
+    <h1>Danial Muahmmad</h1>
+    <h2>H2 nadpis</h2>
+    <h3>H3 nadpis</h3>
+    <h4>H4 nadpis</h4>
+    <h5>H5 nadpis</h5>
+    <h6>H6 nadpis</h6>
+    <span style="color: yellow">Dobrý den</span>
+    <br>
+    <p style= color:black>
+    <b>Tlustý</b>
+    <i>Hubený</i>
+    </p>
+    <a href= https://gitlab.ssemi.cz>Odkaz na Gitlab</a> 
+    
+    <br>
+    <br>
+    <img src="AI" alt="Obrazek AI" width="100" height=100>
+    
+    <br>
+    <b> <span style="color:black">Klikací obrázek odkazující na Stránky Školy</span></b>
+    <br>
+
+    <a href="https://ssemi.cz">
+        <img src="website" alt="website" title="website" width=100 height=150></a>
+    <h1><span style="color:black">Klikací obrázek odkazující na obrázek</span></h1>
+    <a href="AI.jpg">
+        <img src="AI.jpg" alt="AI" title="AI" width=100 height=100>
+        </a>
+</body>
+</html> 
